e508ae0aca58c408ab7d779f4777dac5591979b1
TYDeng <tdeng072@uottawa.ca>
2024-06-18 19:33:09 -0300

    update code

--- public/assets/yatzy_game.js
+++ public/assets/yatzy_game.js
@@ -1,22 +1,46 @@
-class yatzyGame{
+class yatzyGame{//current state(dice) of the game
     constructor() {
-        this.turn = 0; // Current turn number (0, 1, 2, or 3)
+        this.reRoll = 0; // Current Roll(for re-roll) state (0 for first roll, 1, 2)
         this.diceVal = []; // Current values on each of the 5 dice
         this.diceKeep = []; // Keep(or not)  state for dice
     }
 
-    initializeGame() {
-        this.turn = 0;
+    initializeGame() {//initailize as zero state (dice not roll)[suggest with 6 '?' for all dices]
+        this.reRoll = 0;
         this.diceVal = [0, 0, 0, 0, 0]; // Initialize with 5 dice values
-        this.diceKeep = [false, false, false, false, false]; // Initialize all dice to re-roll
+        this.diceKeep = [false, false, false, false, false]; // Initialize all dice not roll
+        //False in dicekeep for re-roll that dice in next turn
     }
+}
 
-    rollDice() {
-        for (let i = 0; i < 5; i++) {
-            if (!this.keepDice[i]) {
-                this.diceVal[i] = diceRoll(); // Roll the dice if not kept
-            }
-        }
+class scoreBoard{//All current score of socreboard
+    constructor(){
+        this.lowerScore = 0;
+        this.upperScore = 0;
+        this.turn = 0;
+        this.diceVal = [];
+        this.diceKeep = [];
+        this.scores = {
+            //uppercase
+            'Ones': 0,
+            'Twos': 0,
+            'Threes': 0,
+            'Fours': 0,
+            'Fives': 0,
+            'Sixes': 0,
+
+            //lowercase
+            'One Pair': 0,
+            'Two Pairs': 0,
+            'Three of a Kind': 0,
+            'Four of a Kind': 0,
+            'Small Straight': 0,
+            'Large Straight': 0,
+            'Full House': 0,
+            'Chance': 0,
+            'Yatzy': 0
+        };
+        this.totalScore = 0;
     }
 
 }
